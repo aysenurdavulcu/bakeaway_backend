49c900ca25530738afbfab94a94ad95304f6a5a8
Awais <razhar5214@bths.edu>
2022-05-14 14:28:00 -0400

    updated database table for bakedgoodsdata to include descriptions for each item

--- database.js
+++ database.js
@@ -27,12 +27,12 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
                     // Table just created, creating some rows
                     var insert = 'INSERT INTO bakedgoodsdata (GoodsID,SellerID,Name,Description,Seller,Price,Zipcode) VALUES (?,?,?,?,?,?,?)';
 
-                    db.run(insert, [20,'101', 'Chocolate Cupcakes', 'description', 'Kate S', '1.25', '11223'])
-                    db.run(insert, [21, '102', 'Red Velvet Cupcakes', 'description','Elaine R', '1.50', '11223'])
-                    db.run(insert, [22, '103', 'Sugar Cookies', 'description', 'James T', '0.99', '11223'])
-                    db.run(insert, [23, '103', 'Cinnamum Buns', 'description', 'Amy R', '2.25', '11223'])
-                    db.run(insert, [24, '103', 'Reeses Cups', 'description','Sarah P', '1.75', '11223'])
-                    db.run(insert, [25, '103', 'Strawberry Cheesecake', 'description','Claire S', '7.95', '11223'])
+                    db.run(insert, [20,'101', 'Chocolate Cupcakes', 'These super moist chocolate cupcakes pack TONS of chocolateflavor in each cupcake wrapper! Made from simple everyday ingredients', 'Kate S', '1.25', '11223'])
+                    db.run(insert, [21, '102', 'Red Velvet Cupcakes', 'Moist and fluffy red velvet cupcakes topped with a big dollop of cream cheese frosting are beyond delicious. There’s something so addictive about cream cheese frosting; sweetness is tempered with a bit of tang, the texture is creamy and luscious','Elaine R', '1.50', '11223'])
+                    db.run(insert, [22, '103', 'Sugar Cookies', 'Sugar Cookies are a sweet and tender cookie with wonderfully crisp edges. They are truly an American favorite', 'James T', '0.99', '11223'])
+                    db.run(insert, [23, '103', 'Cinnamon Buns', 'Sweet baked dough filled with a cinnamon-sugar filling. Made with a rich dough leavened with yeast, their characteristic form is due to rolling a dough sheet containing sweetened cinnamon filling', 'Amy R', '2.25', '11223'])
+                    db.run(insert, [24, '103', 'Reeses Cups', "The classic combination of chocolate and peanut butter, REESE'S Peanut Butter Cups are the perfect companion for movies, sports and parties",'Sarah P', '1.75', '11223'])
+                    db.run(insert, [25, '103', 'Strawberry Cheesecake', 'Bright, beautiful, and luscious Strawberry Cheesecake. This silky smooth cheesecake has a layer of easy homemade strawberry spread on the bottom and then a swirl of it through the top. Fresh strawberry flavors with creamy cheesecake is a perfect pair','Claire S', '7.95', '11223'])
                 }
             })
         db.run(`CREATE TABLE userdata(
